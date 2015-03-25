a42f63ff8d59a9dfebb334ec67d1063fed2d62dc
Nícolas Bouvié da Silva <nicolasbouvie@gmail.com>
2015-03-24 23:35:35 -0300

    openshift postgres connection

--- src/main/java/br/com/nicolas/frameworks/config/DataSourceConfig.java
+++ src/main/java/br/com/nicolas/frameworks/config/DataSourceConfig.java
@@ -2,6 +2,8 @@ package br.com.nicolas.frameworks.config;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.lang3.StringUtils;
+import org.postgresql.ds.PGPoolingDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -10,8 +12,21 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 public class DataSourceConfig {
 	@Bean(name="dataSource")
 	public DataSource getDataSource() {
+		String host = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+		String port = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+		if (StringUtils.isNotBlank(host) && StringUtils.isNoneBlank(port)) {
+			PGPoolingDataSource ds = new PGPoolingDataSource();
+			ds.setDataSourceName("FrameworksDS");
+			ds.setServerName(host);
+			ds.setDatabaseName("frameworks");
+			ds.setPortNumber(Integer.parseInt(port));
+			ds.setUser(System.getenv("OPENSHIFT_POSTGRESQL_DB_USERNAME"));
+			ds.setPassword(System.getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD"));
+			ds.setMaxConnections(10);
+			return ds;
+		}
 		return new EmbeddedDatabaseBuilder()
-			.addDefaultScripts()
-			.build();
+					.addDefaultScripts()
+					.build();
 	}
 }
